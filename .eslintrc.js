3ea2d5744adde3bdd4dd3971138c90a6abd674ca
Dylan Seago <dylan.seago@gmail.com>
2017-10-14 18:54:23 -0400

    Add eslint testing rules and plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,16 @@
 module.exports = {
-    "extends": "airbnb-base"
+  "extends": "airbnb-base",
+  "plugins": [
+    "mocha",
+    "chai-expect"
+  ],
+  "env": {
+    "mocha": true
+  },
+  "rules": {
+    "chai-expect/missing-assertion": 2,
+    "chai-expect/terminating-properties": 1,
+    "no-unused-expressions": 0,
+    "no-param-reassign": 0,
+  }
 };
